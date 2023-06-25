9f2b623fcc4d5ce0eac0c1d80ec9ece9d57ec261
atchougo <atchougo@student.42lyon.fr>
2023-06-25 19:50:00 +0200

    the final project

---
+++ WORDLE/finalProject/includes/Game.hpp
@@ -0,0 +1,41 @@
+//
+// Created by tgriffit on 6/25/23.
+//
+
+#ifndef WORDLE_GAME_HPP
+#define WORDLE_GAME_HPP
+
+#include "main.hpp"
+#include "Letter.hpp"
+
+class Game {
+    size_t lives;
+    bool funMode;
+    std::string wordOfTheDay;
+    std::vector<std::string> dictionary;
+    std::map<char, Letter::e_state>   letterColor;
+
+    bool isWordKnown(std::vector<std::string> &refList, const std::string &word);
+    void checkLetters(const std::string &pword, const std::string &target);
+    size_t getNbSameLettersFound(char c, std::map<size_t , Letter> &checked);
+    size_t getNb1LetterTarget(char c, const std::string &target);
+    std::map<size_t , Letter> getLettersAtCorrectPlace(const std::string &pword, const std::string &target);
+    bool isThereAnOtherLetterToFind(char target, std::map<size_t,Letter> &letters);
+    bool isAtCorrectPlace(char c, size_t cpos, const std::string &target);
+    std::string paintLetter(char c, const std::string &color, Letter::e_state state);
+    void rewriteLine(const std::string &line, const std::string &color, bool isError);
+    void printKeyboardColor(std::string	str);
+    bool printKeyboard();
+
+public:
+    Game();
+    Game(size_t lives, bool funMode, std::vector<std::string> dictionary);
+    ~Game();
+
+    void play();
+    const std::string &getRandomWord(std::vector<std::string> &list, bool funMode);
+
+};
+
+
+#endif //WORDLE_GAME_HPP
---
+++ WORDLE/finalProject/sources/main.cpp
@@ -0,0 +1,107 @@
+#include <cstring>
+#include "../includes/main.hpp"
+#include "../includes/Letter.hpp"
+#include "../includes/Game.hpp"
+
+void	printWelcome(void)
+{
+	std::string	welcomeStr[12] = {
+		"#####################################################################################",
+		"#                                                                                   #",
+		"#         **       **    *******    *******    *******    **        ********        #",
+		"#        /**      /**   **/////**  /**////**  /**////**  /**       /**/////         #",
+		"#        /**   *  /**  **     //** /**   /**  /**    /** /**       /**              #",
+		"#        /**  *** /** /**      /** /*******   /**    /** /**       /*******         #",
+		"#        /** **/**/** /**      /** /**///**   /**    /** /**       /**////          #",
+		"#        /**** //**** //**     **  /**  //**  /**    **  /**       /**              #",
+		"#        /**/   ///**  //*******   /**   //** /*******   /******** /********        #",
+		"#        //       //    ///////    //     //  ///////    ////////  ////////         #",
+		"#                                                                                   #",
+		"#####################################################################################"
+	};
+	for (int i = 0; i < 12; i++)
+		std::cout << CYAN << welcomeStr[i] << RESET << std::endl;
+}
+
+void	printRules(unsigned long dictSize)
+{
+	std::cout << std::endl << std::endl;
+	std::cout << "RULES OF THE GAME:" << std::endl << std::endl;
+	std::cout << "0#   find the WORDLE" << std::endl << std::endl;
+	std::cout << "1#   " << LIVES << " guesses to find the WORDLE" << std::endl;
+	std::cout << "2#   you can only enter " << WORD_SIZE <<" letters WORDLES" << std::endl;
+	std::cout << "3#   the letters can only be lowercase" << std::endl;
+	std::cout << "4#   a "<< GREEN "GREEN" RESET << " letter means it is valid and at the right place in the WORDLE" << std::endl;
+	std::cout << "5#   a " << YELLOW "YELLOW" RESET << " letter means it is valid but not at the right place in the WORDLE" << std::endl;
+	std::cout << "6#   a "<< GREY "GREY" RESET << " letter means it is not valid for the WORDLE" << std::endl;
+    std::cout << "7#   to loose press ENTER without any letter" << std::endl;
+	std::cout << std::endl << "Today the pool of WORDLE size is: " + std::to_string(dictSize) << std::endl;
+	std::cout << std::endl << std::endl;
+}
+
+bool	closeError(std::ifstream *inputStream, std::string msg)
+{
+	inputStream->close();
+	std::cerr << msg << std::endl;
+    return false;
+}
+
+bool    parseDict(std::ifstream *is, std::vector<std::string> &dic, const std::string &path)
+{
+    std::string line;
+    struct stat fileStat;
+    size_t      dictSize = 0;
+
+    if (!is->is_open())
+    {
+        std::cerr << "Error: Could not open dictionnary" << std::endl;
+        return (false);
+    }
+    else if (!(stat(path.c_str(), &fileStat) == 0 && S_ISREG(fileStat.st_mode)))
+        return (closeError(is, "Error: " + path + " is not a regular file"));
+    while(std::getline(*is, line))
+    {
+        dictSize++;
+        if (is->fail())
+			return (closeError(is, "Error: fail while reading"));
+		else if (dictSize > MAX_DICT_SIZE)
+			return (closeError(is, "Error: dictionnary has more than " + std::to_string(MAX_DICT_SIZE) + " entries"));
+		else if (line.size() != 5)
+			return (closeError(is, "Error: dictionnary entry " + std::to_string(dictSize) + " does not have a length of " + std::to_string(WORD_SIZE)));
+		for (unsigned int i = 0; i < line.size(); i++)
+		{
+            line[i] = std::tolower(line[i]);
+			if (!islower(line[i]))
+				return closeError(is, "Error: dictionnary entry " + std::to_string(dictSize) + " does not contain only alphabetic characters");
+		}
+        dic.push_back(line);
+    }
+    if (dictSize < MIN_DICT_SIZE)
+        return (closeError(is, "Error: dictionnary has less than " + std::to_string(MIN_DICT_SIZE) + " entries"));
+    else if (!is->eof() && !is->good())
+		return (closeError(is, "Error: error while reading dictionnary file"));
+    else if (dic.empty())
+        return (closeError(is, "Error: dictionnary is empty"));
+    is->close();
+    return (true);
+}
+
+int main(int argc, char *argv[]) {
+    std::vector<std::string> dic;
+    std::string dicPath = "words.txt";
+    bool funMode = false;
+    if (argc == 2)
+        dicPath = argv[1];
+    if (argc == 3 && !strcmp(argv[2], "true"))
+        funMode = true;
+
+    std::ifstream is(dicPath);
+
+    if (!parseDict(&is, dic, dicPath))
+        return 2;
+    printWelcome();
+    printRules(dic.size());
+    Game game(LIVES, funMode, dic);
+    game.play();
+    return 0;
+}
