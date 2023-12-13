d96402dc6163640bb67e35b7f0bb7c83af9c2c0a
atchougo <atchougo@student.42lyon.fr>
2023-12-13 19:01:40 +0100

    test main for colors

---
+++ FT_SHMUP/includes/EnnemyShip.h
@@ -0,0 +1,24 @@
+#ifndef _ENNEMYSHIP_H_
+#define _ENNEMYSHIP_H_
+
+#include "Ship.h"
+
+class EnnemyShip
+{
+private:
+    /* data */
+public:
+    EnnemyShip(/* args */);
+    ~EnnemyShip();
+};
+
+EnnemyShip::EnnemyShip(/* args */)
+{
+}
+
+EnnemyShip::~EnnemyShip()
+{
+}
+
+
+#endif /* _ENNEMYSHIP_H_ */
